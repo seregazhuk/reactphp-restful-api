eccec0fdb857c0f6b8336ffb2b7d22c0a8be4d53
Zhuk Sergey <seregazhuk88@gmail.com>
2019-10-20 13:57:29 +0300

    Use env in doctrine db config

--- migrations-db.php
+++ migrations-db.php
@@ -1,9 +1,13 @@
 <?php declare(strict_types=1);
 
+require __DIR__ . '/vendor/autoload.php';
+
+\Dotenv\Dotenv::create(__DIR__)->load();
+
 return [
-    'dbname' => 'shop',
-    'user' => 'root',
-    'password' => '',
-    'host' => 'localhost',
-    'driver' => 'pdo_mysql',
+    'dbname' => getenv('DB_NAME'),
+    'user' => getenv('DB_USER'),
+    'password' => getenv('DB_PASS'),
+    'host' => getenv('DB_HOST'),
+    'driver' => getenv('DB_DRIVER'),
 ];
